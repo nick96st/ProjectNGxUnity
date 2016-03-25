823aa9f23dee3043a17ec45596662af802967279
nick96st <nikolay.yotov@abv.bg>
2016-03-25 01:37:42 +0000

    [1]Added player click on object UI popp up scripts: CameraMovement(empty),UItasks(pop up), ControllerMain(to manage clicks)
    [2] added respective test scene
    
    [3] added Tag : Lootable to ProjectSettings

---
+++ ProjectNG/Assets/Scripts/Player/ControllerMain.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+
+public class ControllerMain : MonoBehaviour {
+    // SCRIPT WILL CONTROL THE FLOW OF BASIC INPUT SUCH AS MOUSECLICKS 
+    // 
+
+    // for UI popup
+    UITasks.Task giveTask;
+    public static bool spawnedUI = false;
+    Vector3 mousePos; // to be used where the Ui should appear
+
+    public GameObject ScriptHolder;// reference to other static scripts
+
+	// raycasting 
+	RaycastHit hit1;
+	Ray mouseRay;
+
+	void Start () {
+
+	}
+
+
+	void Update () 
+	{    mousePos = Input.mousePosition;
+		 mouseRay = Camera.main.ScreenPointToRay (mousePos);
+			if(Input.GetMouseButtonDown(0))
+		{
+            Debug.Log(spawnedUI);
+			if(!EventSystem.current.IsPointerOverGameObject()) // blocks click on overlaying UI with object
+			{
+
+			  ScriptHolder.GetComponent<UITasks>().unEnableThis();// set UI script inactive
+
+			if(Physics.Raycast(mouseRay,out hit1,10000))
+				{   UITasks.mousePos = mousePos;
+					UITasks.Target   = hit1.transform.gameObject;
+			
+                    /*
+                    set task to needed where task is the the type of object you clicked
+
+                    */
+							if(hit1.transform.gameObject.tag=="Terrain")
+								{
+									giveTask = UITasks.Task.Ground;
+									Debug.Log("clicked on ground");
+								}
+							else if(hit1.transform.gameObject.tag=="Player")
+								{
+                        giveTask = UITasks.Task.PtoP; 
+									Debug.Log("clicked on player");			
+								}
+							else if(hit1.transform.gameObject.tag=="Lootable")
+								{
+                        giveTask = UITasks.Task.Lootable;//  task 3 =Lootable 
+								Debug.Log("clicked on lootable");
+								//ScriptHolder.g
+								}
+							else if(hit1.transform.gameObject.tag=="NPC")
+								{
+                        giveTask = UITasks.Task.NPC;
+								Debug.Log("clicked on NPC");
+								}
+							else if(hit1.transform.gameObject.tag=="Door")
+								{
+                        giveTask = UITasks.Task.Door;
+									Debug.Log("Door");
+							    }
+			                else {
+                        giveTask = UITasks.Task.Untagged;	
+									Debug.Log("Untagged");
+									}
+                    //	Debug.Log(UITasks.task.ToString());
+                  
+
+                     if (giveTask != UITasks.Task.Untagged)
+                    {
+                        // if it is interactable
+                        spawnedUI = true;
+                        ScriptHolder.GetComponent<UITasks>().SpawnUI(giveTask);
+                    }
+                     // despawn is it doesnt spawn new
+                    else if (spawnedUI == true)
+                    {
+                        spawnedUI = false;
+                        ScriptHolder.GetComponent<UITasks>().unEnableThis();
+
+                    }
+                }
+
+			}
+		}
+	}
+}
---
+++ ProjectNG/Assets/Scripts/Player/UITasks.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+/*  this script instanciates the buttons and apply everything they need to be able to open close
+      
+
+*/ 
+
+public class UITasks : MonoBehaviour {
+	public enum Task{
+		Lootable = 1, // controls for loot : (Loot)/(Unlock)/(SearchDeeply)  1/3
+		Ground   = 2, //move/move and shoot  (Move)/Run&Gun                  1/2   
+		NPC  = 3,     //NPC interaction UI(shoot,talk,daze(if close))        2/3
+		Door = 4,     // control doors     (Lock/unlock)/(Close/Open)        2/2
+		Moveable = 5, // block path or free path Pull*/Push/Left/Right       3/4
+		PtoP     = 6, //Player to Pl trading/treat wounds/Comfort/Encourage  1/4  
+		Untagged = 0 
+	}
+
+
+	//public static Task task;
+	public static GameObject Target;
+	public static Vector3 mousePos;
+
+
+	public GameObject ButtonInstance;
+	RectTransform UIRect=new RectTransform();
+	Rect UIRect1;
+	GUILayout LayoutUI;
+	byte buttonNeeded;
+	string[] ButtonText = new string[4];              //sets text to be filled when buttons get spawned
+	GameObject[] ActionUI = new GameObject[4]; 
+	Button[] ActionUIButton=new Button[4];
+
+	public void unEnableThis()
+	{   
+		for(int j=0;j<4;j++)
+			if(ActionUI[j]!=null)
+		Destroy(ActionUI[j]);
+
+		GameObject.Find ("Main Camera").GetComponent <CameraMovement>().enabled = true;     // allows camera movement
+		this.GetComponent<UITasks> ().enabled = false;
+		}
+
+
+	void Awake () {
+		
+
+		UIRect1.x = mousePos.x;
+		UIRect1.y = mousePos.y;
+		UIRect1.width = Screen.width  / 10;
+		UIRect1.height = Screen.height/ 10;
+
+	
+
+			//ActionUI[i].enabled=false;
+			//ActionUI[i].GetComponent<RectTransform>().=UIRect;
+
+
+		}
+
+
+    public void SpawnUI(Task task)
+	{
+            //block camera movement so it doesnt have to move the UI respectively 
+            GameObject.Find("Main Camera").GetComponent<CameraMovement>().enabled = false;
+            Debug.Log("tASK: " + task.ToString());
+
+
+        /*
+         way to construct 
+         buttonNeeded = number it will need for sure;
+         set those button to their respective names like
+                ButtonText = "text";
+        add ifs for each possible but not sure
+        if(){
+        ButtonText[buttonNeeded] = "text";
+        buttonNeeded++;      
+        } 
+        */
+
+        //examples given
+        if (task == Task.PtoP)
+        {
+            buttonNeeded = 1;
+            ButtonText[buttonNeeded - 1] = "Move";
+        }
+        else if (task == Task.Lootable)
+        {
+            ButtonText[0] = "Loot";
+            ButtonText[1] = "Unlock";
+            ButtonText[2] = "Inspect";
+            buttonNeeded = 3;
+        }
+
+
+        /*   if (STATICS.ScreenResX == 1080)
+           {   
+
+               }*/
+        // Set Size according to Res and ratio;
+        UIRect1.width = Screen.width / 10;
+        UIRect1.height = Screen.height / 100 * 5 + Screen.height % 10;
+
+        // instanciate buttons 
+        for (int i = 0; i < buttonNeeded; i++)
+            {
+              // CARE: this might force garbage collection from time to time since we make new objects
+                ActionUI[i] = GameObject.Instantiate(ButtonInstance, new Vector3(mousePos.x, mousePos.y - UIRect1.height * i, 100), Quaternion.identity) as GameObject;
+                ActionUI[i].transform.SetParent(GameObject.Find("Canvas").transform);
+                //	fixes bugs with SetParent.transform
+                ActionUI[i].GetComponent<RectTransform>().sizeDelta = Vector2.zero;
+                ActionUI[i].GetComponent<RectTransform>().sizeDelta = new Vector2(UIRect1.width, UIRect1.height);
+
+                // setup listener for Onclick to despawn + act
+                ActionUIButton[i] = ActionUI[i].GetComponent<Button>();
+                ActionUIButton[i].GetComponentInChildren<Text>().text = (ButtonText[i]);
+                ActionUIButton[i].onClick.RemoveAllListeners();
+                ActionUIButton[i].onClick.AddListener(() => unEnableThis());
+
+            }
+
+	}
+ 
+
+}
